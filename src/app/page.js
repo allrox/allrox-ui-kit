1d356b8b6651e21b19aa9c2cfb69cc193721ee20
allrox <alexandre@allrox.com.br>
2024-11-24 21:24:10 -0300

    Inclusão de imagem em destaque na seção hero do exemplo

--- src/app/page.js
+++ src/app/page.js
@@ -18,18 +18,25 @@ export default function Home() {
       </Section>
 
       <Section bg="bg-red-800">
-        <h1>Este é um título H1</h1>
-        <h2>Este é um título h2</h2>
-        <h3>Este é um título h3</h3>
-        <h4>Este é um título H4</h4>
-        <h5>Este é um título H5</h5>
-        <h6>Este é um título H6</h6>
-        <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
-        <p>Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+        <Grid cols="md:grid-cols-2">
+          <div className="flex flex-col justify-center content-center">
+            <h1>Este é um título H1</h1>
+            <h2>Este é um título h2</h2>
+            <h3>Este é um título h3</h3>
+            <h4>Este é um título H4</h4>
+            <h5>Este é um título H5</h5>
+            <h6>Este é um título H6</h6>
+            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos.</p>
+            <p>Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
+          </div>
+          <div>
+            <Image src="/mockup.jpg" width={640} height={800} />
+          </div>
+        </Grid>
       </Section>
 
       <Section>
-        
+
         <Grid cols="md:grid-cols-4">
           <div>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</div>
 
@@ -47,23 +54,23 @@ export default function Home() {
         <h3>Here is a subtitle</h3>
         <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
         <Grid cols="md:grid-cols-4">
-        <div className="flex flex-col gap-4">
-          <Image src="/mockup.jpg" alt="Imagem de exemplo" width={640} height={800} />
-          <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
-        </div>
-        <div className="flex flex-col gap-4">
-          <Image src="/mockup.jpg" alt="Imagem de exemplo" width={640} height={800} />
-          <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
-        </div>
-        <div className="flex flex-col gap-4">
-          <Image src="/mockup.jpg" alt="Imagem de exemplo" width={640} height={800} />
-          <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
-        </div>
-        <div className="flex flex-col gap-4">
-          <Image src="/mockup.jpg" alt="Imagem de exemplo" width={640} height={800} />
-          <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
-        </div>
-       
+          <div className="flex flex-col gap-4">
+            <Image src="/mockup.jpg" alt="Imagem de exemplo" width={640} height={800} />
+            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <Image src="/mockup.jpg" alt="Imagem de exemplo" width={640} height={800} />
+            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <Image src="/mockup.jpg" alt="Imagem de exemplo" width={640} height={800} />
+            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
+          </div>
+          <div className="flex flex-col gap-4">
+            <Image src="/mockup.jpg" alt="Imagem de exemplo" width={640} height={800} />
+            <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica.</p>
+          </div>
+
 
         </Grid>
       </Section>
