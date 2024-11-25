463af6faf9f19e45b412082adada769a83d12e03
allrox <alexandre@allrox.com.br>
2024-11-25 20:13:47 -0300

    Ajustes visuais e inclusão do parâmetro props no componente button

--- src/app/components/ui/Button/index.js
+++ src/app/components/ui/Button/index.js
@@ -1,9 +1,9 @@
 import Link from "next/link"
 
-export default function Button({ cta, link, children }) {
+export default function Button({ cta, link, children, props }) {
     return (
         <Link href={link} passHref>
-            <button className="flex flex-row gap-2 bg-white items-center hover:bg-secondary-400 hover:shadow-md text-primary-900 px-6 py-2 rounded-md" >
+            <button className={`flex flex-row gap-2 bg-white justify-center items-center hover:bg-primary-900 hover:text-white text-primary-900 px-6 py-2 rounded-md ${props}`} >
                 {children} {cta}
             </button>
         </Link>
