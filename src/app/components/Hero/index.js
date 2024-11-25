661ef0dc4518e1eba272b7f715222d143e626204
allrox <alexandre@allrox.com.br>
2024-11-25 20:14:14 -0300

    Ajustes na responsividade do componente hero

--- src/app/components/Hero/index.js
+++ src/app/components/Hero/index.js
@@ -1,7 +1,7 @@
 export default function Hero({ children, bg: params, id }) {
     return (
         <div id={id} className={`p-5 ${params}`}>
-            <div className="max-w-screen-xl mx-auto py-4 min-h-screen">
+            <div className="max-w-screen-xl mx-auto py-4 xl:min-h-screen">
                 {children}
             </div>
         </div>
