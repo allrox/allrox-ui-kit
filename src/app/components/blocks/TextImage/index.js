3beab761e2488b9896cb190f6bd28048162b09d9
allrox <alexandre@allrox.com.br>
2024-11-25 19:35:36 -0300

    Criação do componente bloco de imagem e texto

---
+++ src/app/components/blocks/TextImage/index.js
@@ -0,0 +1,13 @@
+import Image from "next/image"
+
+export default function TextImage({ children, image, width, height, alt, order, params }) {
+
+    return (
+        <div className={`grid grid-cols1 lg:grid-cols-2 justify-center gap-8 py-8 ${params}`}>
+            <div className="flex flex-col justify-center">{children}</div>
+            <div className={`flex flex-col shrink-0 ${order}`}>
+                <Image src={image} width={width} height={height} alt={alt} loading="lazy" sizes="(max-width: 768px) 100vw, (max-width: 2560px) 50vw, 33vw"/>
+            </div>
+        </div>
+    )
+}
