9c0ed8f874a6c160185332640183605829989778
Alexandre Rocha <alexandre@allrox.com.br>
2024-11-29 21:20:06 -0300

    Ajustes de CSS e inclusão da propriedade aria-label para critérios de usabilidade

--- src/app/components/ui/Button/index.js
+++ src/app/components/ui/Button/index.js
@@ -1,9 +1,9 @@
 import Link from "next/link"
 
-export default function Button({ cta, link, children, props }) {
+export default function Button({ cta, link, children, arialabel, props }) {
     return (
-        <Link href={link} passHref>
-            <button className={`flex flex-row gap-2 hover:bg-white justify-center items-center border bg-none text-white hover:text-primary-900 px-6 py-2 rounded-md ${props}`} >
+        <Link href={link} passHref aria-label={arialabel}>
+            <button className={`flex flex-row gap-2 hover:bg-white justify-center items-center border bg-none hover:text-primary-900 font-medium px-6 py-2 rounded-md ${props}`}  >
                 {children} {cta}
             </button>
         </Link>
