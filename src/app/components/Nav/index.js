b3cb95381188b0ad070bad77bab21bdf90367c59
Alexandre Rocha <alexandre@allrox.com.br>
2024-11-29 21:23:17 -0300

    Ajustes e melhorias relacionadas a critérios de usabilidade aria

--- src/app/components/Nav/index.js
+++ src/app/components/Nav/index.js
@@ -24,30 +24,29 @@ export default function Nav({ children, logo, alt, width, height, cta, ctalink, 
 
     return (
         <div className={`flex flex-col gap-4 w-full md:flex-row items-center md:justify-between ${bg}`}>
+
             <div className="flex items-center justify-between w-full md:w-auto">
                 <Image src={logo} alt={alt} width={width} height={height} />
                 <div className="px-8 mt-1 ext-base font-bold text-[#FFCD46]">
                     {brandtext}
                 </div>
 
-                <button className="block md:hidden p-2 text-2xl" onClick={() => setMenuOpen(!menuOpen)}>
+                <button className="block md:hidden p-2 text-2xl" aria-label="Menu mobile" onClick={() => setMenuOpen(!menuOpen)}>
                     <RxHamburgerMenu />
                 </button>
             </div>
 
             <ul className={`${menuOpen ? "flex" : "hidden"} flex-col md:flex md:flex-row gap-4 md:gap-8 sm:items-start  md:items-center w-full md:w-auto grow justify-end text-base`}>
-
                 {children}
-
-                {cta && (
-                    <div className="mt-2 md:mt-0">
-
-                        <Button cta={cta} link={ctalink}>
-                            <FaWhatsapp />
-                        </Button>
-                    </div>
-                )}
-
+                <li>
+                    {cta && (
+                        <div className="mt-2 md:mt-0 md:ms-10">
+                            <Button cta={cta} link={ctalink}>
+                                <FaWhatsapp />
+                            </Button>
+                        </div>
+                    )}
+                </li>
             </ul>
         </div>
     )
