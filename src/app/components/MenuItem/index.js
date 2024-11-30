cb615b395f2c9138fc6b65382329d05f23dc0c4a
Alexandre Rocha <alexandre@allrox.com.br>
2024-11-29 21:19:06 -0300

    Ajustes de CSS e passagem de parâmetros do componente

--- src/app/components/MenuItem/index.js
+++ src/app/components/MenuItem/index.js
@@ -1,7 +1,7 @@
-export default function MenuItem({ label, link, target, state }) {
+export default function MenuItem({ label, link, target, props }) {
 
     return (
-        <li className={state}>
+        <li className={`pb-2 border-white/[.1] border-bottom border-1 border-b sm:border-0 sm:pb-0 ${props}`}>
             <a href={link} target={target} >
                 {label}
             </a>
