0a00c6bf030c5f28e3b0254b715cd9d0f31c7a25
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-01 01:23:42 -0300

    Melhorias no componente Card, como inclusão de ícones de funções variadas e ajustes visuais

--- src/app/components/Card/index.js
+++ src/app/components/Card/index.js
@@ -1,19 +1,40 @@
 import Image from "next/image"
+import { CiShare2 } from "react-icons/ci";
+import { CiSettings } from "react-icons/ci";
+import { CiBookmarkPlus } from "react-icons/ci";
 
-export default function Card({ children, imgurl, alt, width, height, title }) {
+export default function Card({ children, imgurl, alt, width, height, title, share, settings, bookmark }) {
     return (
-        <div className="flex flex-col bg-primary-900/[.45] p-4 rounded-lg">
+        <div className="flex flex-col bg-primary-900/[.45] p-4 rounded-lg text-base font-regular">
             <Image
                 src={imgurl}
                 alt={alt}
                 width={width}
                 height={height}
                 className="rounded-md" />
-            <div className="pt-4">
+            <div className="pt-6">
                 {title && (
-                    <span className="text-2xl font-bold text-secondary-400">{title}</span>
+                    <span className="text-3xl font-bold">{title}</span>
                 )}
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-1">
+                    <div className="flex flex-row gap-2 text-2xl mt-4 py-2 border-t border-b border-1 border-white/[0.05]">
+                        {share &&
+                            <a href={share}>
+                                <CiShare2 />
+                            </a>
+                        }
+                        {settings &&
+                            <a href={share}>
+                                <CiSettings />
+                            </a>
+                        }
+                        {bookmark &&
+                            <a href={share}>
+                                <CiBookmarkPlus />
+                            </a>
+                        }
+
+                    </div>
                     {children}
                 </div>
             </div>
