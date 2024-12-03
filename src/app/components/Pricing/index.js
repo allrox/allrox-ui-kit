c2660303a9383764f3bbcd4c7302fae2e0ae2dd9
allrox <alexandre@allrox.com.br>
2024-12-02 23:10:58 -0300

    Ajustes nos cards e passagem da URL para o  botão de ação

--- src/app/components/Pricing/index.js
+++ src/app/components/Pricing/index.js
@@ -1,9 +1,11 @@
 export default function Pricing({ oldprice, price, method }) {
     return (
         <div className="flex flex-col items-start mb-4 h-full">
+            {oldprice &&
                 <div className="text-sm min-h-6">
                     {oldprice}
                 </div>
+            }
             <div className="flex flex-row font-bold items-end text-2xl grow">
                 R$<span className="text-4xl mb-[-2px] mx-1">{price}</span>
                 {method &&
--- src/app/components/ui/Button/index.js
+++ src/app/components/ui/Button/index.js
@@ -1,11 +1,11 @@
 import Link from "next/link"
 
-export default function Button({ cta, link, children, arialabel, props }) {
+export default function Button({ cta, buttonUrl, children, arialabel, props }) {
     return (
-        <Link href={link} passHref aria-label={arialabel}>
+        <a href={buttonUrl} aria-label={arialabel}>
             <button className={`flex flex-row gap-2 hover:bg-white justify-center items-center border bg-none hover:text-primary-900 font-medium px-6 py-2 rounded-md ${props}`}  >
                 {children} {cta}
             </button>
-        </Link>
+        </a>
     )
 }
