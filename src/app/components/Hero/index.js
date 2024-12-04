94ebd3e9f313dcba48790c7efd5354ac83331953
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-03 22:45:22 -0300

    Ajustes na altura da seção Hero

--- src/app/components/Hero/index.js
+++ src/app/components/Hero/index.js
@@ -1,6 +1,6 @@
 export default function Hero({ children, params, id }) {
     return (
-        <div id={id} className={`flex flex-col px-6 ${params}`}>
+        <div id={id} className={`flex flex-col px-6 min-h-svh ${params}`}>
             <div className="max-w-screen-2xl mx-auto">
                 {children}
             </div>
