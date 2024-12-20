8b9972b386586919c9f8d9d8cec7cfc876c43a31
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-19 23:58:12 -0300

    Adiciona classe 'scroll-smooth' e estilo de comportamento de rolagem suave ao elemento HTML

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className='scroll-smooth' style={{scrollBehavior:'smooth'}}>
       <meta name="author" description="@AllRox" />
       <meta name="keywords" description="AllRox, UI, UX, UI Kit, React, Next JS, Tailwind CSS" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
