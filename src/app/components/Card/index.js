3f871175f210b34458c5a8e85853be6f318a3a7e
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-21 22:23:19 -0300

    Add tag support to Card component and update PricingLine layout

--- src/app/components/Card/index.js
+++ src/app/components/Card/index.js
@@ -4,10 +4,15 @@ import { CiSettings } from "react-icons/ci";
 import { CiBookmarkPlus } from "react-icons/ci";
 import PricingLine from "../PricingLine";
 
-export default function Card({  props, imgurl, alt, width, height, title, share, settings, bookmark, desc, oldprice, price, method , buttonUrl}) {
+export default function Card({ props, imgurl, alt, width, height, title, share, settings, bookmark, desc, oldprice, price, method, buttonUrl, tag }) {
     return (
-        <div className={`flex flex-col bg-primary-900/[.45] rounded-xl text-base font-regular  ${props}`}>
-        
+        <div className={`flex flex-col bg-white text-black rounded-xl text-base font-regular  ${props}`}>
+
+            {tag && (
+                <div className="absolute text-xs font-medium mx-4 my-4 bg-primary-600 text-white px-[12] py-[3] rounded-2xl">{tag}</div>
+            )
+            }
+
             <Image
                 src={imgurl}
                 alt={alt}
@@ -15,21 +20,21 @@ export default function Card({  props, imgurl, alt, width, height, title, share,
                 height={height}
                 className="rounded-t-xl" />
 
-            <div className="p-5 ">
+            <div className="p-6 flex flex-col flex-1">
                 {title && (
-                    <span className="text-3xl font-bold">{title}</span>
+                    <span className="text-3xl font-bold text-primary-600">{title}</span>
                 )}
-                <div className="flex flex-col gap-1">
-                    <div className="flex flex-row gap-2 text-2xl mt-4 py-2 border-t border-b border-1 border-white/[0.05]">
+                <div className="flex flex-col gap-1 flex-1">
+                    <div className="flex flex-row gap-3 text-3xl mt-4 py-2 border-t border-b border-1 border-black/[0.2] text-lime-600">
                         {share && <a href={share}><CiShare2 /></a>}
                         {settings && <a href={share}><CiSettings /></a>}
                         {bookmark && <a href={share}><CiBookmarkPlus /></a>}
                     </div>
-                    <div className="flex flex-col h-full justify-around">
-                        <div className="py-4">
+                    <div className="flex flex-col h-full justify-around flex-1">
+                        <div className="py-3 flex-1 text-sm">
                             {desc}
                         </div>
-                        <PricingLine oldprice={oldprice} price={price} method={method} buttonUrl={buttonUrl}/>
+                        <PricingLine oldprice={oldprice} price={price} method={method} buttonUrl={buttonUrl} />
                     </div>
                 </div>
             </div>
