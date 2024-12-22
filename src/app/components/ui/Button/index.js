47a38659627a3cedba5874668d91aed4ceeae947
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-21 22:23:48 -0300

    Update Button component styles for improved appearance and usability

--- src/app/components/ui/Button/index.js
+++ src/app/components/ui/Button/index.js
@@ -1,9 +1,7 @@
-import Link from "next/link"
-
 export default function Button({ cta, buttonUrl, children, arialabel, props }) {
     return (
         <a href={buttonUrl} aria-label={arialabel}>
-            <button className={`flex flex-row gap-2 hover:bg-white justify-center items-center border bg-none hover:text-primary-900 font-medium px-6 py-2 rounded-md ${props}`}  >
+            <button className={`flex flex-row gap-2 justify-center items-center bg-secondary-400 text-lg text-black font-medium px-6 py-4 rounded-lg ${props}`}  >
                 {children} {cta}
             </button>
         </a>
