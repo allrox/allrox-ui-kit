0ca93e9e64fdd5c89e2c8b7f1add98ec23f9b535
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-28 17:05:55 -0300

    Refactor TextImage component layout for improved alignment and responsiveness

--- src/app/components/blocks/TextImage/index.js
+++ src/app/components/blocks/TextImage/index.js
@@ -5,11 +5,12 @@ export default function TextImage({ children, image, width, height, alt, order, 
     return (
         <div className={`grid grid-cols-1 lg:grid-cols-2 justify-center gap-10 py-8 ${props}`}>
             <div className="flex flex-col">{children}</div>
-            <div className={`flex flex-col items-center ${order}`}>
-                <Image src={image} width={width} height={height} alt={alt} loading="lazy" className={`rounded-lg flex w-full ${maxwidth}`}  />
-                {desc && (
-                    <p className="py-0 my-3">{desc}</p>
-                )
+            <div className={`flex flex-col items-center justify-center ${order}`}>
+                <Image src={image} width={width} height={height} alt={alt} loading="lazy" className={`flex rounded-lg w-full ${maxwidth}`} />
+                {
+                    desc && (
+                        <p className="py-0 my-3">{desc}</p>
+                    )
                 }
             </div>
         </div>
