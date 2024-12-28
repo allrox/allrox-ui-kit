518611d508e8806808e101f233f377958a5f84cd
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-28 17:06:30 -0300

    Refactor PricingLine component to integrate CartButton and display offerTag for enhanced pricing presentation

--- src/app/components/PricingLine/index.js
+++ src/app/components/PricingLine/index.js
@@ -1,20 +1,31 @@
-import Button from "../ui/Button"
+import CartButton from "../CartButton"
+import { MdOutlineDiscount } from "react-icons/md"
 
-export default function PricingLine({ price, method, buttonUrl, oldprice }) {
+export default function PricingLine({ price, offerTag, buttonUrl, buttonIcon, oldprice, props }) {
   return (
-    <div className="flex flex-col gap-2 border-t border-1 border-white/[0.05] pt-4 h-full]">
+    <div className={`flex flex-col gap-2 border-t border-1 border-white/[0.05] pt-4 h-full] ${props}`}>
       {
         oldprice &&
-        <span className="line-through text-base">
+        <span className="line-through text-base font-bold text-red-400">
           from {oldprice}
         </span>
       }
 
-      <div className="flex flex-wrap items-center gap-4">
-        <Button props="flex grow" cta={`Buy for ${price}`} buttonUrl={buttonUrl} />
-        <div className="flex text-sm font-bold gap-1 grow animate-bounce">
-          {method}
+      <div className="flex flex-wrap items-center gap-6">
+
+        <div className="grow">
+          <CartButton cta={`Buy for ${price}`} buttonUrl={buttonUrl} props="w-full">
+            {buttonIcon}
+          </CartButton>
         </div>
+
+        {
+          offerTag && (
+          <div className="flex text-sm text-primary-700 font-medium gap-1 animate-bounce mx-auto items-center">
+            <MdOutlineDiscount /> {offerTag}
+          </div>
+        )}
+
       </div>
     </div>
   )
