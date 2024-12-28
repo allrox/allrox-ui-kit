1fd7e457d5f8205129e42428c40041c498d6e9df
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-27 22:17:38 -0300

    Enhance TextImage component by adding description support and improving image styling for better layout

--- src/app/components/blocks/TextImage/index.js
+++ src/app/components/blocks/TextImage/index.js
@@ -1,12 +1,16 @@
 import Image from "next/image"
 
-export default function TextImage({ children, image, width, height, alt, order, params }) {
+export default function TextImage({ children, image, width, height, alt, order, props, desc }) {
 
     return (
-        <div className={`grid grid-cols1 lg:grid-cols-2 justify-center gap-10 py-8 ${params}`}>
+        <div className={`grid grid-cols-1 lg:grid-cols-2 justify-center gap-10 py-8 ${props}`}>
             <div className="flex flex-col">{children}</div>
             <div className={`flex flex-col shrink-0 ${order}`}>
-                <Image src={image} width={width} height={height} alt={alt} loading="lazy" sizes="(max-width: 768px) 100vw, (max-width: 2560px) 50vw, 33vw"/>
+                <Image src={image} width={width} height={height} alt={alt} loading="lazy" className="rounded-lg flex w-full" sizes="(max-width: 768px) 100vw, (max-width: 2560px) 50vw, 33vw" />
+                {desc && (
+                    <p className="py-0 my-3">{desc}</p>
+                )
+                }
             </div>
         </div>
     )
