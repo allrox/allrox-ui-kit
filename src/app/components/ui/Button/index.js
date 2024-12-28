8a4fd0c62f49656b674076f92982cd3bc2d5ba86
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-28 17:06:47 -0300

    Refactor Button component to support buttonIcon prop and improve styling for better visual consistency

--- src/app/components/ui/Button/index.js
+++ src/app/components/ui/Button/index.js
@@ -1,8 +1,14 @@
-export default function Button({ cta, buttonUrl, children, arialabel, props }) {
+export default function Button({ cta, buttonUrl, children: buttonIcon, arialabel, props }) {
     return (
         <a href={buttonUrl} aria-label={arialabel}>
-            <button className={`flex gap-2 justify-center items-center bg-secondary-400 text-lg text-black font-medium px-6 py-4 rounded-lg ${props}`}  >
-                {children} {cta}
+            <button className={`flex gap-2 grow bg-secondary-400 text-md text-black font-bold px-8 py-4 rounded-lg items-center ${props}`}  >
+                {buttonIcon &&
+                    (
+                        <div>
+                            {buttonIcon}
+                        </div>
+                    )}
+                {cta}
             </button>
         </a>
     )
