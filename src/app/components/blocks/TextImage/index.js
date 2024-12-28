c02c3866d3ac2b523ed38618317068e47464af81
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-27 22:52:10 -0300

    Enhance TextImage component by adding maxwidth prop and improving layout for better responsiveness

--- src/app/components/blocks/TextImage/index.js
+++ src/app/components/blocks/TextImage/index.js
@@ -1,12 +1,12 @@
 import Image from "next/image"
 
-export default function TextImage({ children, image, width, height, alt, order, props, desc }) {
+export default function TextImage({ children, image, width, height, alt, order, props, desc, maxwidth }) {
 
     return (
         <div className={`grid grid-cols-1 lg:grid-cols-2 justify-center gap-10 py-8 ${props}`}>
             <div className="flex flex-col">{children}</div>
-            <div className={`flex flex-col shrink-0 ${order}`}>
-                <Image src={image} width={width} height={height} alt={alt} loading="lazy" className="rounded-lg flex w-full" sizes="(max-width: 768px) 100vw, (max-width: 2560px) 50vw, 33vw" />
+            <div className={`flex flex-col items-center ${order}`}>
+                <Image src={image} width={width} height={height} alt={alt} loading="lazy" className={`rounded-lg flex w-full ${maxwidth}`}  />
                 {desc && (
                     <p className="py-0 my-3">{desc}</p>
                 )
