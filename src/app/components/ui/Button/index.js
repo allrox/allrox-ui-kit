85c16338588d0458d05a6a151c54b2016e5bf8c5
Alexandre Rocha <alexandre@allrox.com.br>
2024-12-27 22:16:00 -0300

    Refactor Button component to remove unnecessary flex property for improved layout consistency

--- src/app/components/ui/Button/index.js
+++ src/app/components/ui/Button/index.js
@@ -1,7 +1,7 @@
 export default function Button({ cta, buttonUrl, children, arialabel, props }) {
     return (
         <a href={buttonUrl} aria-label={arialabel}>
-            <button className={`flex flex-row gap-2 justify-center items-center bg-secondary-400 text-lg text-black font-medium px-6 py-4 rounded-lg ${props}`}  >
+            <button className={`flex gap-2 justify-center items-center bg-secondary-400 text-lg text-black font-medium px-6 py-4 rounded-lg ${props}`}  >
                 {children} {cta}
             </button>
         </a>
