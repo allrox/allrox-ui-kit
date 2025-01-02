896e84ae38cf3697aee334677fe4e9090ee7e873
Alexandre Rocha <alexandre@allrox.com.br>
2025-01-02 17:40:06 -0300

    Add @vercel/analytics for enhanced performance tracking and integrate into layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata = {
   title: "AllRox: The UI Kit Project",
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
